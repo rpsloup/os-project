ef37b87b17ad597fd0ca193bb99e491134040d6a
rpsloup <rpsloup@proton.me>
2023-02-20 15:06:18 +0100

    FE: Adding and setting up Prettier

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,7 @@
-const App = (): JSX.Element =>
-  (
-    <>
-      <h1>Initial Commit</h1>
-    </>
-  );
+const App = (): JSX.Element => (
+  <>
+    <h1>Initial Commit</h1>
+  </>
+);
 
 export default App;
