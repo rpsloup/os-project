941fd5720dbc6d51b161ec7f167751645dea1a2a
rpsloup <rpsloup@proton.me>
2023-02-21 09:21:30 +0100

    BE: Adding and setting up Express

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,1 +1,13 @@
-console.log('Hello, world!');
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const DEFAULT_PORT = 3001;
+const port = process.env.PORT || DEFAULT_PORT;
+
+app.listen(port, () => {
+  console.log(`[STATUS] Listening on port ${port}...`);
+});
