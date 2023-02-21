5192f94cc6a82ddc3656f2e3a442222d33260f79
rpsloup <rpsloup@proton.me>
2023-02-21 10:12:59 +0100

    FE: Styling the Taskbar component and adding a position prop

--- client/src/components/Desktop/Desktop.component.tsx
+++ client/src/components/Desktop/Desktop.component.tsx
@@ -1,13 +1,21 @@
 import Taskbar from '../Taskbar';
 
+import type { TaskbarPosition } from '../../typings/taskbarTypes';
+
 import './Desktop.styles.scss';
 
 interface IProps {
   showTaskbar?: boolean;
+  taskbarPosition?: TaskbarPosition;
 }
 
-const Desktop = ({ showTaskbar = true }: IProps): JSX.Element => (
-  <div className="desktop">{showTaskbar ? <Taskbar /> : null}</div>
+const Desktop = ({
+  showTaskbar = true,
+  taskbarPosition,
+}: IProps): JSX.Element => (
+  <div className="desktop">
+    {showTaskbar ? <Taskbar position={taskbarPosition} /> : null}
+  </div>
 );
 
 export default Desktop;
--- client/src/components/Taskbar/Taskbar.component.tsx
+++ client/src/components/Taskbar/Taskbar.component.tsx
@@ -1,3 +1,13 @@
-const Taskbar = (): JSX.Element => <div className="taskbar">Taskbar</div>;
+import type { TaskbarPosition } from '../../typings/taskbarTypes';
+
+import './Taskbar.styles.scss';
+
+interface IProps {
+  position?: TaskbarPosition;
+}
+
+const Taskbar = ({ position }: IProps): JSX.Element => (
+  <div className={`taskbar${position ? ` taskbar--${position}` : ''}`} />
+);
 
 export default Taskbar;
